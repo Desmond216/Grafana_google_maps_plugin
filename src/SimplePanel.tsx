512638f08e15281c469a9352277222228090ae34
zhenjiesoh@gmail.com <zhenjiesoh@gmail.com>
2022-08-04 15:27:35 +0800

    adding initial google map

--- src/SimplePanel.tsx
+++ src/SimplePanel.tsx
@@ -2,48 +2,85 @@ import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { SimpleOptions } from 'types';
 import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import { FieldSet, stylesFactory, useTheme } from '@grafana/ui';
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+
+
 
 interface Props extends PanelProps<SimpleOptions> {}
 
+const containerStyle = {
+  width: '100%',
+  height: '400px'
+};
+
+
+
+const MyComponent = () => {
+
+
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyCkWHYr7iuvWaRYIMcqa6kg-BISnDCnfDg"
+  })
+
+  const [map, setMap] = React.useState(null)
+
+  const onLoad = React.useCallback(function callback(map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map)
+  }, [])
+
+  const center = {
+    lat: -3.745,
+    lng: -38.523
+  };
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null)
+  }, [])
+
+  const createKey = (location) => {
+    return location.lat + location.long
+  }
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+
+    >
+      { /* Child components, such as markers, info windows, etc. */ }
+      <>
+        <Marker key={createKey(center)} position={center}></Marker>
+      </>
+    </GoogleMap>
+) : <></>
+}
+
+
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
+  const radii = data.series
+    .map(series => series.fields.find(field => field.type === 'number'))
+    .map(field => field?.values.get(field.values.length - 1));
+
+  
+  
+  const latList = data.series
+  .map(series => series.fields.find(field => series.fields.indexOf(field) === 1))
+  .map(field => field?.values.get(field.values.length - 1));
+  const lngList = data.series.find(series => data.series.indexOf(series) === 0)
   const theme = useTheme();
   const styles = getStyles();
-  return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
 
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
-        )}
-        <div>Text option value: {options.text}</div>
-      </div>
+  return (
+    <div>
+      hi {radii}
+      <MyComponent/>
     </div>
   );
 };
